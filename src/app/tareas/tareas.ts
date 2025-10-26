53eb6b997c16cfad6438d019b2da056661d21b54
rodgar00 <rodrigogarciaperez00@gmail.com>
2025-10-26 21:58:26 +0100

    Ejercicio acabado

--- src/app/tareas/tareas.ts
+++ src/app/tareas/tareas.ts
@@ -6,18 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./tareas.scss']
 })
 export class Tareas {
-  mostrar = false; // controla si la clase disable se aplica
-
-  // Array de tareas
-  tareas = [
-    { nombre: 'Tarea 1', val: true },
-    { nombre: 'Tarea 2', val: true },
-    { nombre: 'Tarea 3', val: true },
-    { nombre: 'Tarea 4', val: true },
-    { nombre: 'Tarea 5', val: true }
-  ];
+  noMostrar: boolean = false;
 
   toggle() {
-    this.mostrar = !this.mostrar;
+    this.noMostrar = !this.noMostrar;
   }
 }
